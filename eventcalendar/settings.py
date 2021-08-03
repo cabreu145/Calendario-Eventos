869b1e01d7163e5fac7760ddd8b797b730745883
Carlos Abreu <35485515+cabreu145@users.noreply.github.com>
2021-08-03 12:05:54 -0500

    CAmbios

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -119,12 +119,14 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+TIME_INPUT_FORMATS = ('%I:%M %p',)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static")
+    
+]
